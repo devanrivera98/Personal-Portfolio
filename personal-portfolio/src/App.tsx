c46875bc9f18b8d0e5b4be01a26de2f17962320c
Devan Rivera <devanrivera98@gmail.com>
2023-10-13 00:36:51 +0000

    using vite instead of cra

---
+++ personal-portfolio/src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  // const backgroundStyle = {
+  //   backgroundImage: `url(/background.jpg)`,
+  //   backgroundSize: 'cover',
+  //   backgroundPosition: 'center',
+  //   height: '100vh',
+  // };
+
+  return (
+    <div className='background-style'>
+      <div className="App">
+        <div className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App

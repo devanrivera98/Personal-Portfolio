1dcb635fb3d15ee8bb8917cb8d9c5c6e80b004f4
Devan Rivera <devanrivera98@gmail.com>
2023-10-17 22:01:50 +0000

    styling the header

--- portfolio-website/components/Header.tsx
+++ portfolio-website/components/Header.tsx
@@ -6,20 +6,19 @@ export default function Header() {
 
   return (
     <>
-      <div className='background-style'>
-        <div>
-          <div className="navbar">
-            <ul>
-              <li className="bg-blue-500">Home</li>
-              <li>Portfolio</li>
-              <li>About Me</li>
-              <li>Contact</li>
-              <button className="bg-blue-600 rounded-full">Download CV</button>
-            </ul>
-          </div>
+      <header className="background-style">
+        <div className="navbar flex items-center justify-between">
+          <ul className="flex items-center space-x-4">
+            <li>Devan</li>
+            <li className="hover:bg-sky-600 hover:underline hover:underline-offset-7">Home</li>
+            <li>Portfolio</li>
+            <li>About Me</li>
+            <li>Contact</li>
+          </ul>
+          <button className="bg-blue-600 rounded-full p-2">Download CV</button>
         </div>
         <Outlet />
-      </div>
+      </header>
     </>
   )
 }

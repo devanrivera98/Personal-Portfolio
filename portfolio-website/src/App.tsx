c224ab51e4ac5423bb7c551bbd1121b3bc8c21f9
Devan Rivera <devanrivera98@gmail.com>
2023-10-17 00:18:36 +0000

    created new configuration due to issues with vite

---
+++ portfolio-website/src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <div className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App

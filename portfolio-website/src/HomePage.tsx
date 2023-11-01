3c10a4283f4aee6c37221ba72df45a639d255693
Devan Rivera <devanrivera98@gmail.com>
2023-11-01 22:36:50 +0000

    visual changes to the homepage

--- portfolio-website/src/HomePage.tsx
+++ portfolio-website/src/HomePage.tsx
@@ -6,11 +6,13 @@ export default function HomePage() {
 
   return (
     <div id="home">
-      <div className="background-style text-center pt-5 mx-auto flex flex-col md:flex-row items-center">
-        <div className="w-full md:w-1/2 md:pr-8">
-          <h1 className="text-4xl">Hello, I'm Devan Rivera</h1>
-          <p className="pt-5 px-4 text-2xl">A Software Engineer from the vibrant city of Los Angeles, California, fueled by a boundless passion for creating innovative solutions.</p>
-          <div className="flex justify-center pt-5 w-full">
+      <div className="background-style text-start pt-5 mx-auto flex justify-center md:flex-row">
+        <div className="w-1/2 custom-homepage">
+          <h1 className="text-xl text-lightGreen pb-3">Hello, my name is</h1>
+          <p className="md:text-5xl text-3xl pb-3">Devan Rivera.</p>
+          <p className="md:text-5xl text-3xl text-lightBlue pb-5">I Transform Ideas into Pixels.</p>
+          <p className="pt-5 md:mr-20 text-start">A Software Engineer from the vibrant city of Los Angeles, California, fueled by a boundless passion for creating innovative solutions.</p>
+          <div className="flex justify-start pt-5 w-full homepage-icons">
             <button className="px-2 border-2 border-lightGreen hover:bg-green-400 hover:text-white rounded text-lightGreen">Download CV</button>
             <div className="flex w-12 mx-2">
               {/* <p className="px-2">LinkedIn</p> */}
@@ -22,9 +24,9 @@ export default function HomePage() {
             </div>
           </div>
         </div>
-        <div className="p-6 flex justify-center md:w-1/2 profile-image">
+        {/* <div className="p-6 flex justify-center md:w-1/2 profile-image">
           <img className="w-3/4 md:w-full" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png" alt="stock image" />
-        </div>
+        </div> */}
       </div>
       <AboutSection/>
       <MyProjects/>

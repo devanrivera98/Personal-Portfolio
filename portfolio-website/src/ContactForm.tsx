dd3d74d881802173f05234d1056f385ced4322ff
Devan Rivera <devanrivera98@gmail.com>
2023-11-04 18:57:14 +0000

    outlined the mobile view of contact form

--- portfolio-website/src/ContactForm.tsx
+++ portfolio-website/src/ContactForm.tsx
@@ -7,23 +7,26 @@ export default function ContactForm () {
 
   console.log(name, email, message)
   return (
-    <div>
-      <form name="react-contact-form" method="POST" >
-        <input type="hidden" name="form-name" value="contact"></input>
-        <div className="input">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" placeholder="Your Name" onChange={(e) => setName(e.target.value)} />
-        </div>
-        <div className="input">
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="input">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" placeholder="Your Message" onChange={(e) => setMessage(e.target.value)}></textarea>
-        </div>
-        <button className="contact-background" type="submit">Send Message</button>
-      </form>
+    <div className="bg-lightBlue pb-5">
+      <h1 className="text-3xl text-center py-5">Contact Me</h1>
+      <div>
+        <form className="flex flex-col gap-5" name="react-contact-form" method="POST" >
+          <input type="hidden" name="form-name" value="contact"></input>
+          <div className="input flex flex-col">
+            <label className="ml-5 pb-1" htmlFor="name">Name</label>
+            <input className="mx-5 pl-1 rounded" type="text" id="name" name="name" placeholder="John Doe" onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className="input flex flex-col">
+            <label className="ml-5 pb-1" htmlFor="email">Email</label>
+            <input className="mx-5 rounded pl-1" type="text" id="email" name="email" placeholder="johndoe@gmail.com" onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div className="input flex flex-col">
+            <label className="ml-5 pb-1" htmlFor="message">Message</label>
+            <textarea className="mx-5 rounded pl-1" name="message" id="message" placeholder="Type your message here" onChange={(e) => setMessage(e.target.value)}></textarea>
+          </div>
+          <button className="contact-background mx-5" type="submit">Send Message</button>
+        </form>
+      </div>
     </div>
   )
 }

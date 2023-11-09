3db59f4354e05a79ca41325ec1c9818c92f93b84
Devan Rivera <devanrivera98@gmail.com>
2023-11-09 00:39:40 +0000

    fixed meta tags

--- portfolio-website/src/HomePage.tsx
+++ portfolio-website/src/HomePage.tsx
@@ -2,7 +2,6 @@ import AboutSection from "./AboutSection"
 import ContactForm from "./ContactForm"
 import MyFooter from "./MyFooter"
 import MyProjects from "./MyProjects"
-import { Helmet } from 'react-helmet-async';
 export default function HomePage() {
 
 
@@ -10,27 +9,6 @@ export default function HomePage() {
   return (
     <>
     <div id="home">
-        <Helmet>
-          {/* <!-- Primary Meta Tags --> */}
-          <title>Devan Rivera's Portfolio</title>
-          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-          <meta name="title" content="Devan Rivera's Portfolio" />
-          <meta name="description" content="Welcome to my personal portfolio! Explore my projects and feel free to get in touch. I'm excited to share my work with you – take a look!" />
-
-          {/* <!-- Open Graph / Facebook --> */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://devanrivera.netlify.app/" />
-          <meta property="og:title" content="Devan Rivera's Portfolio" />
-          <meta property="og:description" content="Welcome to my personal portfolio! Explore my projects and feel free to get in touch. I'm excited to share my work with you – take a look!" />
-          <meta property="og:image" content="/meta-image.png" />
-
-          {/* <!-- Twitter --> */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content="https://devanrivera.netlify.app/" />
-          <meta property="twitter:title" content="Devan Rivera's Portfolio" />
-          <meta property="twitter:description" content=" Welcome to my personal portfolio! Explore my projects and feel free to get in touch. I'm excited to share my work with you – take a look!" />
-          <meta property="twitter:image" content="/meta-image.png" />
-        </Helmet>
       <div id="home-1" className="background-style text-start pt-5 mx-auto flex justify-center md:flex-row">
         <div className="w-1/2 custom-homepage">
           <h1 className="text-xl text-lightGreen pb-3">Hello, my name is</h1>

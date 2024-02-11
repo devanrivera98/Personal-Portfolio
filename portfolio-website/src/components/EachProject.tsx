90624312ab29a1e8f8eee6a2f8623033a9f01d15
Devan Rivera <devanrivera98@gmail.com>
2024-02-11 00:06:35 +0000

    got the proper spacing needed for multiple pills

--- portfolio-website/src/components/EachProject.tsx
+++ portfolio-website/src/components/EachProject.tsx
@@ -3,32 +3,32 @@ export default function EachProject({ title, image, url, description } : {title:
 
     return (
       <>
-        <div className="md:w-1/3 border-4 border-blue-200 m-5 rounded">
+        <div className="md:w-1/3 border-4 border-blue-200 m-5 rounded flex flex-col">
           <h2 className="text-2xl pl-1 bg-lightBlue">{title}</h2>
           <div className="h-52 md:h-64">
             <img className="w-full h-full object-cover" src={image} />
           </div>
-          <div className="px-4">
+          <div className="px-4 flex flex-col flex-1">
             <div className="py-3 flex justify-center">
               <a href={url} target="_blank" rel="noopener noreferrer">
                 <button className="border border-lightGreen hover:bg-green-400 hover:text-white text-lightGreen rounded p-1">Link to Delopyment</button>
               </a>
             </div>
             <p className="text-center">{description}</p>
-            <div className="flex py-3 flex-wrap justify-evenly">
-              <div className="pill-button w-1/4">
+            <div className="flex py-3 flex-wrap justify-evenly my-auto">
+              <div className="pill-button lg:w-1/4  md:w-1/3 w-1/4">
                 <p>React</p>
               </div>
-              <div className="pill-button w-1/4">
+              <div className="pill-button lg:w-1/4  md:w-1/3 w-1/4">
                 <p>React</p>
               </div>
-              <div className="pill-button w-1/4">
+              <div className="pill-button lg:w-1/4  md:w-1/3 w-1/4">
                 <p>React</p>
               </div>
-              <div className="pill-button w-1/4">
+              <div className="pill-button lg:w-1/4  md:w-1/3 w-1/4">
                 <p>React</p>
               </div>
-              <div className="pill-button w-1/4">
+              <div className="pill-button lg:w-1/4  md:w-1/3 w-1/4">
                 <p>React</p>
               </div>
 

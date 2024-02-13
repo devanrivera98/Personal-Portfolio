f3e822f58197dd34bcfa37c0ce77690bf7291231
Devan Rivera <devanrivera98@gmail.com>
2024-02-13 00:31:40 +0000

    added documentation for each skill

--- portfolio-website/src/TechSkills.tsx
+++ portfolio-website/src/TechSkills.tsx
@@ -8,21 +8,21 @@ export default function TechSkills() {
           <h2>Technical Skills</h2>
         </div>
         <div className="w-2/3 flex flex-wrap justify-center mx-auto pt-5">
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" title="React" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" title="JavaScript" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" title="TypeScript" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" title="Node.js" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" title="Express" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" title="PostgreSQL" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" title="Tailwind" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" title="Bootstrap" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" title="CSS3" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" title="HTML5" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" title="Jest" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" title="Vite" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" title="Git" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" title="Github" />
-          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" title="VS Code" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" doc="https://react.dev/" title="React" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" title="JavaScript" doc="https://developer.mozilla.org/en-US/docs/Web/JavaScript" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-original.svg" title="TypeScript" doc="https://www.typescriptlang.org/docs/"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" title="Node.js" doc="https://nodejs.org/docs/latest/api/"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" title="Express" doc="https://expressjs.com/" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" title="PostgreSQL" doc="https://www.postgresql.org/docs/" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" title="Tailwind" doc="https://tailwindcss.com/docs/installation"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" title="Bootstrap" doc="https://getbootstrap.com/docs/4.1/getting-started/introduction/"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original-wordmark.svg" title="CSS3" doc="https://developer.mozilla.org/en-US/docs/Web/CSS"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original-wordmark.svg" title="HTML5" doc="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" title="Jest" doc="https://jestjs.io/docs/getting-started"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" title="Vite" doc="https://vitejs.dev/guide/" />
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" title="Git" doc="https://git-scm.com/doc"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" title="Github" doc="https://docs.github.com/en"/>
+          <EachSkill image="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vscode/vscode-original.svg" title="VS Code" doc="https://code.visualstudio.com/docs"/>
         </div>
     </div>
     </>
--- portfolio-website/src/components/EachSkill.tsx
+++ portfolio-website/src/components/EachSkill.tsx
@@ -1,11 +1,15 @@
-export default function EachSkill({image, title}: {image:string, title:string} ) {
+export default function EachSkill({image, title, doc}: {image:string, title:string, doc:string} ) {
   return (
     <>
       <div className="bg-white lg:w-2/12  rounded-xl flex flex-col items-center m-3">
         <div className="p-2 h-44 lg:h-auto">
-          <img className="w-full h-full" src={image} />
+          <a href={doc} target="_blank" rel="noopener noreferrer">
+            <img className="w-full h-full" src={image} />
+          </a>
         </div>
-        <h1 className="text-xl">{title}</h1>
+        <a href={doc} target="_blank" rel="noopener noreferrer">
+          <h1 className="text-xl">{title}</h1>
+        </a>
       </div>
     </>
   )
